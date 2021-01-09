e3c80220d9d9791406a3776b6799cca0a8f24d4f
Pierguido Lambri <plambri@redhat.com>
2021-01-09 11:47:41 +0000

    First implementation of rwc
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,12 +1,28 @@
 use std::env;
 use std::process;
+use std::fs::File;
+use std::io::{self, BufRead};
 
-fn main() {
+fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Too few arguments.");
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let file = File::open(filename)?;
+    let mut lines = 0;
+    let mut words = 0;
+    let mut chars = 0;
+    for line in io::BufReader::new(file).lines() {
+        let l = line?;
+        lines += 1;
+        words += l.split(' ').count();
+        chars += l.chars().count();
+    }
+    println!("lines: {}", lines);
+    println!("words: {}", words);
+    println!("chars: {}", chars);
+        
+    Ok(())
 }
